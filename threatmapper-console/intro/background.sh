5c515015486c0b10c23c360ee883dacaf4108f3f
Sangam Biradar <sangam14@users.noreply.github.com>
2022-11-20 10:50:06 +0530

    add

---
+++ threatmapper-console/intro/background.sh
@@ -0,0 +1,4 @@
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
